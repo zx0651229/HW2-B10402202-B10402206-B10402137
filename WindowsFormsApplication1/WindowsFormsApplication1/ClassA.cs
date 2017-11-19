f112fcbfe011d5b48e1aafdb6eb751af836b1c90
zx0651229 <zx0651229@gmail.com>
2017-11-19 16:09:14 +0800

    problem1

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ClassA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class ClassA
+    {
+        public int GetNumber()
+        {
+            return 0;
+        }
+    }
+}
